fc9820d0bf0d576b7f01e8f5f1e11eb6f248a7af
maki-nebu <161030007+maki-nebu@users.noreply.github.com>
2026-08-31 13:34:08 -0700

    Remove docblock for update method
    Removed the docblock for the update method.

--- BlogController.php
+++ BlogController.php
@@ -119,13 +119,7 @@ class BlogController extends Controller
         }
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function update(Request $request, $id)
     {
         try {
